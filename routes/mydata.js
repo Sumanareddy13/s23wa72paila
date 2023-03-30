100a19d78e841bbb7e14f68787f7750763f21140
Sumanasri Reddy <pailasumanasri@gmail.com>
2023-03-29 23:19:27 -0500

    "added data"

---
+++ routes/mydata.js
@@ -0,0 +1,9 @@
+var express = require('Sumanasri Paila');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
